141e428648126363631c42657657191e751a969a
AniruddhSringeri <47109291+AniruddhSringeri@users.noreply.github.com>
2019-08-16 21:00:21 +0530

    Update OneDimensionalArray.java

--- OneDimensionalArray.java
+++ OneDimensionalArray.java
@@ -17,7 +17,8 @@ class OneDimensionalArray{
       do{ 
       for(i=0;i<size-1;i++){
         if(arr[i]>arr[i+1]){ temp=arr[i]; arr[i]=arr[i+1]; arr[i+1]=temp; ++flag;}
-       }}
+       }
+      flag=0;}
       while(flag!=0);
       
    
